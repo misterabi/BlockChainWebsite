f3a8ed9462ca2a3f33c240bc7e56aedf890397d2
Kabongo <leonardo.kabongo@gmail.com>
2024-01-26 12:12:54 +0100

    rebase dev

---
+++ src/page/Sign/index.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import { useSDK } from '@metamask/sdk-react'
+import './style.css'
+
+export default function index() {
+  const [account, setAccount] = useState()
+  const { sdk, connected, connecting, provider, chainId } = useSDK()
+
+  const connect = async () => {
+    try {
+      const accounts = await sdk?.connect()
+      setAccount(accounts?.[0])
+    } catch (err) {
+      console.warn(`failed to connect..`, err)
+    }
+  }
+
+  return (
+    <div className="App">
+      <button style={{ padding: 10, margin: 10 }} onClick={connect}>
+        Connect
+      </button>
+      {connected && (
+        <div>
+          <>
+            {chainId && `Connected chain: ${chainId}`}
+            <p></p>
+            {account && `Connected account: ${account}`}
+          </>
+        </div>
+      )}
+    </div>
+  )
+}
