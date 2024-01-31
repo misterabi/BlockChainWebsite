72f1c5f4aa00ad196b6f5956b96272ac32bc475e
Shirley Berchel <berchel.shirley@gmail.com>
2024-01-31 14:59:49 +0100

    tmp: structure of timeline without styles

--- src/page/Roadmap/index.js
+++ src/page/Roadmap/index.js
@@ -1,6 +1,20 @@
 import React from "react";
 import { Box, Container, Typography } from "@mui/material";
 
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+import Groups2Icon from '@mui/icons-material/Groups2';
+import BuildIcon from '@mui/icons-material/Build';
+import TrendingUpIcon from '@mui/icons-material/TrendingUp';
+import DesignServicesIcon from '@mui/icons-material/DesignServices';
+import TimelineIcon from '@mui/icons-material/Timeline';
+import LaunchIcon from '@mui/icons-material/Launch';
+import BrowserUpdatedIcon from '@mui/icons-material/BrowserUpdated';
+import CodeIcon from '@mui/icons-material/Code';
+
+import Icons from '@mui/material/Icon';
+
 import {
   Timeline,
   TimelineConnector,
@@ -10,12 +24,15 @@ import {
   TimelineSeparator,
 } from "@mui/lab";
 
+// import GetIconComponent from "components/utils/GetIconComponent";
+
 function Roadmap() {
     const roadmapData = [
         {
             timeline : 'Q1',
             steps : 'Définition de la Vision et des Objectifs du Projet',
             description : ['Identifier les besoins éducatifs et les problèmes liés à la contrefaçon de diplômes.', 'Définir clairement la vision à long terme et les objectifs spécifiques de DiploEra.'],
+            icon : VisibilityIcon,
         }, 
         {
             timeline : 'Q1-Q2',
@@ -24,12 +41,14 @@ function Roadmap() {
                 'Identifier les cas d\'utilisation spécifiques de DiploEra dans la certification éducative.', 
                 'Développer des scénarios concrets d\'application du service.', 
             ],
+            icon : AssignmentIcon,
         },
         {
             timeline : 'Q1-Q2',
             steps: 'Développement du Modèle Tokenomics',
             description: ['Concevoir un modèle économique basé sur le token de DiploEra, détaillant son utilisation, sa distribution, et son impact sur l\'écosystème.',
             ],
+            icon: AccountBalanceIcon,
         },
         {
             timeline : 'Q2',
@@ -39,6 +58,7 @@ function Roadmap() {
                 'Recruter et intégrer les membres clés de l\'équipe technique, marketing, et opérationnelle.',
                 'Former le personnel sur les aspects techniques et opérationnels du projet.',
             ],
+            icon : Groups2Icon,
         },
         {
             timeline : 'Q2-Q3',
@@ -46,6 +66,7 @@ function Roadmap() {
             description : [
                 'Concevoir et développer un prototype initial de DiploEra pour valider la faisabilité technique.', 
                 'Explorer les fonctionnalités clés, notamment l\'émission, la vérification, et le stockage sécurisé des diplômes.'],
+            icon: BuildIcon,
         },
         {
             timeline : 'Q2-Q3',
@@ -54,6 +75,7 @@ function Roadmap() {
                 'Élaborer une stratégie de marketing qui englobe le lancement, la sensibilisation et la croissance continue.',
                 'Établir un plan détaillé pour le développement continu, les mises à jour et la maintenance du projet.',
             ],
+            icon: TrendingUpIcon,
         },
         {
             timeline : 'Q3-Q4',
@@ -61,6 +83,7 @@ function Roadmap() {
             description: [
                 'Développer l\'architecture complète de DiploEra, en tenant compte des aspects de sécurité, d\'interopérabilité, et d\'évolutivité.',
                 'Affiner les fonctionnalités et l\'expérience utilisateur.'],
+                icon: DesignServicesIcon,
         },
         {
             timeline : 'Q3 - Année Suivante',
@@ -68,6 +91,7 @@ function Roadmap() {
             description : [
                 'Établir un plan détaillé pour le développement continu, les mises à jour et la maintenance du projet',
             ],
+            icon: CodeIcon,
         },
         {
             timeline : 'Q4-Année suivante',
@@ -76,6 +100,7 @@ function Roadmap() {
                 'Élargir le Proof of Concept pour inclure des tests plus étendus et des retours d\'utilisateurs.',
                 'Identifier et résoudre tout problème identifié pendant cette phase.',
             ],
+            icon: TimelineIcon,
         },
         {
             timeline : 'Année suivante',
@@ -84,6 +109,7 @@ function Roadmap() {
                 'Déployer officiellement DiploEra sur le réseau blockchain, permettant aux utilisateurs d\'émettre, partager, et vérifier des diplômes.',
                 'Lancer des campagnes marketing pour promouvoir l\'adoption de la plateforme.'
             ],
+            icon: LaunchIcon,
         },
         {
             timeline: 'Tout au Long du Projet',
@@ -91,50 +117,49 @@ function Roadmap() {
             description: [  
                 'Planifier des révisions régulières de la roadmap pour refléter les progrès réalisés, les nouvelles opportunités et les ajustements stratégiques éventuels.',
             ],
+            icon: BrowserUpdatedIcon,
         },
     ]
+    roadmapData.map((item, index) => {
+        console.log(index,item);
+    })
+
+    //Fonction utilitaire
+    const getIconComponent = (iconName) => {
+        const icon = Icons[iconName];
+        if (icon) {
+          return icon;
+        } else {
+          // Gérer le cas où le nom de l'icône n'est pas trouvé
+          return null;
+        }
+      };
   return (
     <Container maxWidth="large">
       <Box>
         <Typography>Header</Typography>
       </Box>
       <Box>
-        {/* <Timeline position="alternate">
-          <TimelineItem>
-            <TimelineSeparator>
-              <TimelineDot />
-              <TimelineConnector />
-            </TimelineSeparator>
-            <TimelineContent>
-              <Typography variant="h6" component="span">
-                (Q1) Définition de la Vision et des Objectifs du Projet
-              </Typography>
-              <Typography>
-                Identifier les besoins éducatifs et les problèmes liés à la
-                contrefaçon de diplômes.
-              </Typography>
-              <Typography>
-                Définir clairement la vision à long terme et les objectifs
-                spécifiques de DiploEra.
-              </Typography>
-            </TimelineContent>
-          </TimelineItem>
-        </Timeline> */}
         <Timeline position="alternate">
           {roadmapData.map((item, index) => (
             <TimelineItem key={index}>
               <TimelineSeparator>
-                <TimelineDot />
+                <TimelineDot>
+                    {/* Icon composant created dynamically */}
+                    {<item.icon sx={{
+                        fontSize: 40,
+                    }} />}
+                </TimelineDot>
                 {index < roadmapData.length - 1 && <TimelineConnector />}
               </TimelineSeparator>
-              <TimelineContent>
-                <Typography variant="h6" component="span">
-                  ({item.timeline}) {item.title || item.steps}
+            <TimelineContent  sx={{ py: '12px', px: 2, my: '18px'}} >
+                <Typography variant="h5" component="span">
+                ({item.timeline}) {item.title || item.steps}
                 </Typography>
                 {item.description.map((desc, i) => (
-                  <Typography key={i}>{desc}</Typography>
+                <Typography key={i}>{desc}</Typography>
                 ))}
-              </TimelineContent>
+            </TimelineContent>
             </TimelineItem>
           ))}
         </Timeline>
